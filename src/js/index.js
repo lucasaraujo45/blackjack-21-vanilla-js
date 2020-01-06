2feda20ddf8ca53bdc89f6bd3094bdd575e97dab
Lucas Dearaujo <lucasaraujo45@outlook.com>
2020-01-06 21:32:52 +0000

    last touch ups

--- src/js/index.js
+++ src/js/index.js
@@ -6,18 +6,32 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = () => {
-    document.querySelector(".card").classList.add("generateRandomSuit");
-    document.querySelector(".card").innerHTML.add("generateRandomNumber");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
 
-let generateRandomNumber(){
-    let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
-    let indexNumbers = Math.floor(Math.random() * numbers.length);
-    return numbers[indexNumbers];
-}
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
 
-let generateRandomSuit(){
-    let suit = ["diamond", "spade", "heart","club"];
-    let indexSuit = Math.floor(Math.random() * suit.length);
-    return suit[indexSuit];
-}
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
